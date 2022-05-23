043ecba48770334e4473e2c1b827089211d47cfd
AmirMc231 <amirmc231@gmail.com>
2022-05-22 20:23:08 -0700

    Lesson B

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private const float spawnRate = 1.5f;
     public List<GameObject> prefabs;
+
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0); 
     }
 
     IEnumerator SpawnTarget()
     {
         while (true)
         {
-            yield return new WaitForSeconds(spawnRate); 
+            yield return new WaitForSeconds(spawnRate);
+            //UpdateScore(1);
             Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
         }
         //return ;
@@ -25,5 +32,16 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int scoreDelta)
+    {
+        score += scoreDelta;
+        //score = score + scoreDelta;
+        if(score < 0)
+        {
+            score = 0;
+        }
+        scoreText.text = " Score: " + score;
     }
 }
