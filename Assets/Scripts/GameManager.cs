bf914fd1faa66adb32bd2daf5602fd468a9720bc
AmirMc231 <amirmc231@gmail.com>
2022-05-22 21:21:27 -0700

    Lesson C

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -9,18 +12,30 @@ public class GameManager : MonoBehaviour
     public List<GameObject> prefabs;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameoverText;
+    public Button restartButton;
     private int score = 0;
+    public bool gameActive = true;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
-        UpdateScore(0); 
+        UpdateScore(0);
+
+        
     }
 
+    public void GameOver()
+    {
+        gameoverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        gameActive = false;
+
+    }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (gameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             //UpdateScore(1);
@@ -43,5 +58,10 @@ public class GameManager : MonoBehaviour
             score = 0;
         }
         scoreText.text = " Score: " + score;
+    }
+
+    public void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
